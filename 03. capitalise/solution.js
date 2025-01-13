8d613cb9a72634bfcc620209e5e0dd31ecf68e93
Sobana <108879379+sobanaq@users.noreply.github.com>
2025-01-13 12:33:45 +0000

    test passed:solution

--- 03. capitalise/solution.js
+++ 03. capitalise/solution.js
@@ -1,6 +1,6 @@
 function capitalise(word) {
-  word[0] = word[0].toUpperCase();
-  return word;
+ return word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase();
+  
 }
 
 module.exports = { capitalise };
